1bd7b96de39dca85f4f26b325277bcf5f48bdb22
fengjiandong <ddshiyu@163.com>
2025-06-16 20:18:22 +0800

    新增新的证书

--- server/utils/index.js
+++ server/utils/index.js
@@ -3,9 +3,9 @@ const WxPay = require('wechatpay-node-v3');
 // 微信支付实例，放在路由外部全局定义，避免重复创建
 const pay = new WxPay({
   appid: process.env.WECHAT_APPID,
-  mchid: process.env.WECHAT_MCHID,
-  publicKey: fs.readFileSync(__dirname + '/../wxpay_pem/apiclient_cert.pem'), // 公钥
-  privateKey: fs.readFileSync(__dirname + '/../wxpay_pem/apiclient_key.pem'), // 秘钥
+  mchid: process.env.WECHAT_MCHID_2,
+  publicKey: fs.readFileSync(__dirname + '/../wxpay_pem_2/apiclient_cert.pem'), // 公钥
+  privateKey: fs.readFileSync(__dirname + '/../wxpay_pem_2/apiclient_key.pem'), // 秘钥
 });
 
 module.exports = {
